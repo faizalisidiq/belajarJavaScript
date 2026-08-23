0a83f74b3199aadfb29a0a3dae6119d12f2d014a
Faiz Ali Sidiq <faiz161104@gmail.com>
2026-08-23 23:54:01 +0700

    Ternary Operator

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-const testArray = [1, 2, 3, 4, 5, 100]
+let nilai = 85;
 
-const angka = testArray.includes(6)
-const posisiAngka = testArray.indexOf(100)
-
-if (angka) {
-    console.log(posisiAngka)
-} else {
-    console.log('Tidak ada array yang dimaksud')
-}
+let status = nilai >= 75 ? "A" : "B";
+console.log(status)
