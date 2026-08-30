c9b5bada03a8ce6878098f8ad211f7143e2d513c
Faiz Ali Sidiq <faiz161104@gmail.com>
2026-08-30 10:09:36 +0700

    .map()

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
-let mahasiswa = {
-  name: "Faiz Ali Sidiq",
-  jurusan: "Teknik Informatika",
-  age: 22,
-  gender: "Laki-laki",
-  salam: function () {
-    return `Halo nama saya adalah ${this.name}`;
+// let mahasiswa = {
+//   name: "Faiz Ali Sidiq",
+//   jurusan: "Teknik Informatika",
+//   age: 22,
+//   gender: "Laki-laki",
+//   salam: function () {
+//     return `Halo nama saya adalah ${this.name}`;
+//   },
+// };
+
+// for (let i in mahasiswa) {
+//   console.log(`${i} : ${mahasiswa[i]}`);
+// }
+
+// console.log(mahasiswa.salam());
+
+let produk = [
+  {
+    name: "Laptop",
+    price: 10,
+    stock: 10,
   },
-};
+  {
+    name: "Hp",
+    price: 120,
+    stock: 5,
+  },
+  {
+    name: "Tablet",
+    price: 200,
+    stock: 8,
+  },
+];
 
-for (let i in mahasiswa) {
-  console.log(`${i} : ${mahasiswa[i]}`);
-}
+let namaProduk = produk.map(item => item.name)
+console.log(namaProduk);
 
-console.log(mahasiswa.salam());
+// for (let i = 0; i < produk.length; i++) {
+//   console.log(`\nNama Produk: ${produk[i].name}`);
+//   console.log(`Harga Produk: ${produk[i].price}`);
+//   console.log(`Stok Produk: ${produk[i].stock}`);
+// }
